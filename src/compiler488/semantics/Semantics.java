d389dd6d034b9334ee21ba617eac7d4278249a49
Andriy Dzikh <andriydzikh@gmail.com>
2019-10-28 12:07:36 -0400

    Implemented the call actions.

--- src/compiler488/semantics/Semantics.java
+++ src/compiler488/semantics/Semantics.java
@@ -1,18 +1,20 @@
 package compiler488.semantics;
 
 import java.io.*;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
 import compiler488.ast.AST;
 import compiler488.ast.decl.*;
+import compiler488.ast.expn.FunctionCallExpn;
+import compiler488.ast.expn.IdentExpn;
 import compiler488.ast.stmt.*;
-import compiler488.ast.type.BooleanType;
 import compiler488.symbol.Symbol;
 import compiler488.symbol.SymbolTable;
 import javafx.util.Pair;
+
+import static compiler488.symbol.Symbol.DataType.None;
 
 /** Implement semantic analysis for compiler 488 
  *  @author  <B> Put your names here </B>
@@ -38,6 +40,8 @@ public class Semantics {
 	private List<Symbol> partsAwaitingType;
 
 	private Symbol lastRoutine = null;
+	private Symbol checkingCall = null;
+	private int callArg = 0;
      
      /** SemanticAnalyzer constructor */
 	public Semantics (){
@@ -100,7 +104,7 @@ public class Semantics {
 	 *  Perform one semantic analysis action
          *  @param  actionNumber  semantic analysis action number
          */
-	public void semanticAction( int actionNumber, AST target ) {
+	public void semanticAction( int actionNumber, AST target ) throws Exception {
 
 	if( traceSemantics ){
 		if(traceFile.length() > 0 ){
@@ -139,21 +143,25 @@ public class Semantics {
 		   handleScopeActions(actionNumber, target);
 	   } else if(actionNumber < 20 || actionNumber == 46 || actionNumber == 47) {
 		   handleDeclActions(actionNumber, target);
-	   } else if(actionNumber == 55 || actionNumber == 56) {
+	   } else if(actionNumber >= 55) {
 		   handleSpecialActions(actionNumber);
-	   } else if(actionNumber >= 50 && actionNumber <= 54) {
+	   } else if(actionNumber >= 50) {
 		   handleStatementActions(actionNumber, target);
+	   } else if(actionNumber >= 40 && actionNumber <= 45) {
+		   handleCallActions(actionNumber, target);
 	   }
 
 	   System.out.println("Semantic Action: S" + actionNumber  );
 	   return ;
 	}
 
-	private void handleSpecialActions(int actionNumber) {
+	private void handleSpecialActions(int actionNumber) throws Exception {
 		if(actionNumber == 55) {
 			funcInfo.set(funcInfo.size() - 1, new Pair<>(funcInfo.peek().getKey(), funcInfo.peek().getValue() + 1));
 		} else if(actionNumber == 56) {
 			funcInfo.set(funcInfo.size() - 1, new Pair<>(funcInfo.peek().getKey(), funcInfo.peek().getValue() - 1));
+		} else if(actionNumber == 57) {
+			throw new Exception("Variable is not a routine or a variable!");
 		}
 	}
 
@@ -226,9 +234,9 @@ public class Semantics {
 		if(actionNumber == 50) {
 			assert funcInfo.peek().getValue() > 0;
 		} else if(actionNumber == 51) {
-			assert funcInfo.peek().getKey().resultantType != Symbol.DataType.None;
+			assert funcInfo.peek().getKey().resultantType != None;
 		} else if(actionNumber == 52) {
-			assert funcInfo.peek().getKey().resultantType == Symbol.DataType.None;
+			assert funcInfo.peek().getKey().resultantType == None;
 		} else if(actionNumber == 53) {
 			ExitStmt exit = (ExitStmt)target;
 			assert 0 < exit.getLevel() && exit.getLevel() <= funcInfo.peek().getValue();
@@ -243,5 +251,67 @@ public class Semantics {
 			}
 			assert retStmt != null;
 		}
+	}
+
+	private void handleCallActions(int actionNumber, AST target) {
+		if(actionNumber == 40) {
+			Symbol sym;
+			if(target instanceof FunctionCallExpn) {
+				FunctionCallExpn call = (FunctionCallExpn)target;
+				 sym = getScopeSymbol(call.getIdent());
+			} else {
+				// Therefore, this must be an IdentExpn
+				IdentExpn call = (IdentExpn)target;
+				sym = getScopeSymbol(call.getIdent());
+			}
+			assert sym.type == Symbol.SymbolType.Routine;
+			assert sym.resultantType != None;
+		} else if(actionNumber == 41) {
+			ProcedureCallStmt call = (ProcedureCallStmt)target;
+			Symbol sym = getScopeSymbol(call.getName());
+			assert sym.type == Symbol.SymbolType.Routine;
+			assert sym.resultantType == None;
+		} else if(actionNumber == 42) {
+			Symbol sym;
+			if(target instanceof IdentExpn) {
+				IdentExpn call = (IdentExpn)target;
+				sym = getScopeSymbol(call.getIdent());
+			} else {
+				// So it must be a ProcedureCallStmt
+				ProcedureCallStmt call = (ProcedureCallStmt)target;
+				sym = getScopeSymbol(call.getName());
+			}
+			assert sym.parameters != null && sym.parameters.size() == 0;
+		} else if(actionNumber == 43) {
+			Symbol sym;
+			int providedCount;
+			if(target instanceof FunctionCallExpn) {
+				FunctionCallExpn call = (FunctionCallExpn) target;
+				sym = getScopeSymbol(call.getIdent());
+				providedCount = call.getArguments().size();
+			} else {
+				// So it must be a ProcedureCallStmt
+				ProcedureCallStmt call = (ProcedureCallStmt)target;
+				sym = getScopeSymbol(call.getName());
+				providedCount = call.getArguments().size();
+			}
+			assert sym.parameters != null && sym.parameters.size() == providedCount;
+		} else if(actionNumber == 44) {
+			if(target instanceof FunctionCallExpn) {
+				FunctionCallExpn call = (FunctionCallExpn) target;
+				checkingCall = getScopeSymbol(call.getIdent());
+			} else {
+				// So it must be a ProcedureCallStmt
+				ProcedureCallStmt call = (ProcedureCallStmt)target;
+				checkingCall = getScopeSymbol(call.getName());
+			}
+			callArg = 0;
+		} else if(actionNumber == 45) {
+			callArg++;
+		}
+	}
+
+	public Symbol getScopeSymbol(String name) {
+		return scopes.peek().getValue().getSymbol(name);
 	}
 }
