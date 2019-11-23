311954289f5dee804c6f6a0037cc73fa994c4ee9
andriyDev <andriydzikh@gmail.com>
2019-11-22 19:37:48 -0500

    Added tests. Improving the Test script

--- RUNALLTESTS.sh
+++ RUNALLTESTS.sh
@@ -2,5 +2,16 @@
 echo -e "===== Tests =====\n"
 for filename in tests/*.488; do
     echo -e "----- Case \"" $filename "\" -----\n"
-    java -jar dist/compiler488.jar $filename
+    RESULT=$(echo 7 12 | java -jar dist/compiler488.jar $filename 2> .tmp)
+    DEBUG=$(cat .tmp)
+    startmsp=$(echo "$DEBUG" | sed -e "1,1d" | head -n 1 | cut -d' ' -f9)
+    endmsp=$(echo "$DEBUG" | sed -e "1,5d" | head -n 1 | cut -d' ' -f8)
+    if [ "$startmsp" = "$endmsp" ]
+    then
+        echo ""
+    else
+    fi
+    echo "StartMSP: " $startmsp
+    echo "EndMSP: " $endmsp
+    echo "$RESULT"
 done
