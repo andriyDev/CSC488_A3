5639867a926bba5204ba3531acf54047afe1df7c
andriyDev <andriydzikh@gmail.com>
2019-11-25 13:20:36 -0500

    Made the test script nicer.

--- RUNALLTESTS.sh
+++ RUNALLTESTS.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
+
+repeat() {
+    for i in $(seq 1 $1)
+    do
+        echo -n "."
+    done
+}
+
+MAXNAMELEN=0
+for filename in tests/*.488; do
+    length=${#filename}
+    if [ $MAXNAMELEN \< $length ]
+    then
+        MAXNAMELEN=$length
+    fi
+done
+MAXNAMELEN=$((MAXNAMELEN + 10))
+
 echo -e "===== Tests =====\n"
 for filename in tests/*.488; do
-    echo -e "----- Case \"" $filename "\" -----\n"
+    msg="Test \"$filename\""
+    echo -n -e $msg
+    length=${#msg}
+    repeat $((MAXNAMELEN - length))
+
     RESULT=$(echo 7 12 | java -jar dist/compiler488.jar $filename 2> .tmp)
     DEBUG=$(cat .tmp)
     startmsp=$(echo "$DEBUG" | sed -e "1,1d" | head -n 1 | cut -d' ' -f9)
     endmsp=$(echo "$DEBUG" | sed -e "1,5d" | head -n 1 | cut -d' ' -f8)
-    if [ "$startmsp" = "$endmsp" ]
+    execResult=$(echo "$RESULT" | sed -e "1,2d" | head -c -18)
+    
+    if [ "$startmsp" = "$endmsp" ] && [ "<$execResult>" = $'<End of Compilation\nBegin Execution>' ]
     then
-        echo ""
+        echo "PASSED"
     else
+        echo "FAILED"
     fi
-    echo "StartMSP: " $startmsp
-    echo "EndMSP: " $endmsp
-    echo "$RESULT"
 done
 rm .tmp
